917ad36d221f0c1145c44a08e600136a7e361f1a
PyColors <patweb44@gmail.com>
2021-07-13 21:16:31 +0200

    Add assets folder

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import EmployeeApplication from "./components/EmployeeApplication";
+
+ReactDOM.render(
+    <EmployeeApplication />,
+  document.getElementById('root')
+);
